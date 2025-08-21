6d41abb0dd2a86236c2435c6be26ae9396de3d72
Adam <allshoez88@gmail.com>
2025-08-21 20:47:03 +0700

    Add files via upload
    Signed-off-by: Adam <allshoez88@gmail.com>

---
+++ cd /sdcard/tiktok/videos.js
@@ -0,0 +1,28 @@
+const allVideos = {
+  "Funny": [
+    {
+      "src": "videos/Funny/ssstik.io_@dandiiriantoo_1755679092723.mp4",
+      "title": "ssstik.io_@dandiiriantoo_1755679092723"
+    },
+    {
+      "src": "videos/Funny/ssstik.io_@lumiere3925_1755678402183.mp4",
+      "title": "ssstik.io_@lumiere3925_1755678402183"
+    },
+    {
+      "src": "videos/Funny/ssstik.io_@petparadise278_1755677986251.mp4",
+      "title": "ssstik.io_@petparadise278_1755677986251"
+    }
+  ],
+  "Kids": [
+    {
+      "src": "videos/Kids/ssstik.io_@byjenjenn_1755679168593.mp4",
+      "title": "ssstik.io_@byjenjenn_1755679168593"
+    }
+  ],
+  "Music": [
+    {
+      "src": "videos/Music/ssstik.io_@animal_dance5_1755677847067.mp4",
+      "title": "ssstik.io_@animal_dance5_1755677847067"
+    }
+  ]
+};
